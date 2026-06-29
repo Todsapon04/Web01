13c6faf1a09d74ceab469f665ca20bb2c62fbbbc
3 <todsapon.0330@gmail.com>
2026-06-29 12:03:50 +0700

    Add files via upload

---
+++ WS03/script.js
@@ -0,0 +1,11 @@
+ for(i=1;i<=5;i++){
+    document.write("<h",i,">มหาวิทยาลัยราชภัฏนครปฐม </h",i,">")
+}
+
+
+for (i = 1; i <= 10; i++) {
+    for (j = 1; j <= i; j++) {
+        document.write("<img src='images/1051402e3521b5ced1cefc7935e422fd.jpg' width=70>");
+    }
+    document.write("<br>");
+}
